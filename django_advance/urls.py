0cde3bd1665308055c742007d560a80b47ceb22c
MoonGyuBin <2019captain@gmail.com>
2022-10-28 17:05:20 +0900

    init project

---
+++ django_advance/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+]
